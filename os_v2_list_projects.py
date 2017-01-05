743f79fab9a8ce461f5dc4600a663db80913fc9a
Diogenes S. Jesus <splashx@users.noreply.github.com>
2017-01-05 14:54:43 +0100

    list projects

---
+++ os_v2_list_projects.py
@@ -0,0 +1,27 @@
+# @splash
+# inspired by http://stackoverflow.com/questions/15525827/is-it-possible-to-ge-the-list-of-tenants-a-user-is-associated-with-in-openstack
+#
+# in v2, lists all projects the user has access to without requiring admin privileges
+
+from keystoneclient.v2_0 import client
+from keystoneclient.v2_0 import tokens
+import os
+import requests
+import json 
+
+username = os.environ.get('OS_USERNAME')
+password = os.environ.get('OS_PASSWORD')
+auth_url = os.environ.get('OS_AUTH_URL')
+
+# keystone = client.Client(username=username, password=password, tenant_name=tenant_name, auth_url=auth_url)
+keystone = client.Client(username=username, password=password, auth_url=auth_url)
+token = keystone.auth_token
+headers = {'X-Auth-Token': token }
+tenant_url = auth_url
+tenant_url += '/tenants'
+r = requests.get(tenant_url, headers=headers)
+tenants_raw = r.text
+tenant_data = json.loads(tenants_raw)
+
+for tenant in tenant_data['tenants']: 
+   print tenant['name']
